a71ec6b0f8221dc28c14fe507b6f156bcf773717
matsumoto <dsongben@gmail.com>
2025-08-30 12:06:34 +0900

    localeの設定

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Configure locale and timezone for Japan */
+    locale: 'ja-JP',
+    timezoneId: 'Asia/Tokyo',
   },
 
   /* Configure projects for major browsers */
